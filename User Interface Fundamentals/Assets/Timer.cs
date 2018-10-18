d8fd8f001a8ccdc876173f592027bfffbd3411e2
Edward Goodhew <ejgoodhew@btinternet.com>
2018-10-18 11:21:59 +0100

    test

---
+++ User Interface Fundamentals/Assets/Timer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour {
+
+    private Text TimerTXT;
+    public float Time; 
+
+
+	// Use this for initialization
+	void Start ()
+    {
+        TimerTXT = GameObject.Find("Timer").GetComponent<Text>();
+        
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    private void FixedUpdate()
+    {
+        Timer
+    }
+}
---
+++ User Interface Fundamentals/Assets/Ui_test.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Ui_test : MonoBehaviour {
+
+    // public GameObject TheText;
+    private Text TXT_COMP;
+
+	// Use this for initialization
+	void Start () {
+        TXT_COMP = GameObject.Find("Text").GetComponent<Text>();
+        //TXT_COMP.text = "The Cake Is a Lie";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void SetText()
+    {
+        TXT_COMP.text = "The Cake Is a Lie";
+    }
+
+    public void SetText(string T)
+    {
+        TXT_COMP.text = T;
+    }
+}
